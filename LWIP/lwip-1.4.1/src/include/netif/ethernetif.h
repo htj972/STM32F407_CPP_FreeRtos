42aad17316910c4c309677a91de963b4565c2884
kokirika <htj@kokirika.cn>
2023-07-20 19:44:29 +0800

    EC20 mqtt连接

---
+++ LWIP/lwip-1.4.1/src/include/netif/ethernetif.h
@@ -0,0 +1,21 @@
+#ifndef __ETHERNETIF_H__
+#define __ETHERNETIF_H__
+#include "lwip/err.h"
+#include "lwip/netif.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+//Íø¿¨µÄÃû×Ö
+#define IFNAME0 'e'
+#define IFNAME1 'n'
+ 
+
+err_t ethernetif_init(struct netif *netif);
+err_t ethernetif_input(struct netif *netif);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
